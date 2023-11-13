afcc53bbd25c7763f9c996b0ddaf9592a7a17c63
Ivan Nikitin <ivannikitin9853@gmail.com>
2023-11-13 22:31:00 +0700

    update files, made all queries

--- homework-1/main.py
+++ homework-1/main.py
@@ -12,24 +12,49 @@ conn = psycopg2.connect(
     password=pass_)
 
 
+#
+#
+# def import_data_to_db(file, table):
+#     with open(file, 'r') as fp:
+#         csv_reader = csv.reader(fp)
+#         next_ = next(csv_reader)
+#         try:
+#             with conn.cursor() as cur:
+#                 # execute query
+#                 insert_query = f"INSERT INTO {table} VALUES ({','.join(['%s']*len(next_))})"
+#
+#                 # input data to db
+#                 for row in csv_reader:
+#                     cur.execute(insert_query, row)
+#
+#         finally:
+#             conn.commit()
+#             conn.close()
+#
+#
+# import_data_to_db('./north_data/customers_data.csv', 'customers')
+# import_data_to_db('./north_data/employees_data.csv', 'employees')
+# import_data_to_db('./north_data/orders_data.csv', 'orders')
+
+
 def import_data_to_db(file, table):
     with open(file, 'r') as fp:
         csv_reader = csv.reader(fp)
         next_ = next(csv_reader)
-        try:
-            with conn.cursor() as cur:
-                # execute query
-                insert_query = f"INSERT INTO {table} VALUES ({','.join(['%s']*len(next_))})"
-
-                # input data to db
-                for row in csv_reader:
-                    cur.execute(insert_query, row)
-
-        finally:
-            conn.commit()
-            conn.close()
+        with conn.cursor() as cur:
+            sql = f"INSERT INTO {table} VALUES ({','.join(['%s'] * len(next_))})"
+            for row in csv_reader:
+                cur.execute(sql, row)
 
 
-import_data_to_db('./north_data/customers_data.csv', 'customers')
-import_data_to_db('./north_data/employees_data.csv', 'employees')
-import_data_to_db('./north_data/orders_data.csv', 'orders')
+# conn = psycopg2.connect(...)
+try:
+    import_data_to_db('./north_data/customers_data.csv', 'customers')
+    import_data_to_db('./north_data/employees_data.csv', 'employees')
+    import_data_to_db('./north_data/orders_data.csv', 'orders')
+    conn.commit()
+except:
+    conn.rollback()
+    raise
+finally:
+    conn.close()
